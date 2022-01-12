a9bd9cb816e71fdbf9f98f79463c97897d30464b
Claucia Cambruzzi <cambruzziclaucia@gmail.com>
2022-01-12 19:43:54 +0000

    Add event to button

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
-console.log("Hello, world!");
+const questionContainer = document.querySelectorAll(".question-container button");
+
+for (let i = 0; i < questionContainer.length; i++) {
+  questionContainer[i].onclick = showAnswer;
+}
+
+function showAnswer(event) {
+  const container = event.target.parentElement.parentElement;
+  container.classList.toggle("collapsed");
+  container.classList.toggle("expanded");
+}
