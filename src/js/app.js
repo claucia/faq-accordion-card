4f31bc22aea1e3d1f9c6d6989e54b6b3549bc8b1
Claucia Cambruzzi <cambruzziclaucia@gmail.com>
2022-01-13 09:52:44 +0000

    Fix toggle event

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
-const questionContainer = document.querySelectorAll(".question-container button");
+const questionContainer = document.querySelectorAll(".question-container");
 
 for (let i = 0; i < questionContainer.length; i++) {
   questionContainer[i].onclick = showAnswer;
 }
 
 function showAnswer(event) {
-  const container = event.target.parentElement.parentElement;
+  const container = event.target.closest(".question-and-answer-container");
   container.classList.toggle("collapsed");
   container.classList.toggle("expanded");
 }
